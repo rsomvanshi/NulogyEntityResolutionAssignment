6a8a35d09a02b5fcc68295bcade9d3244c05e9b8
Rohan Somvanshi <rsomvanshi@nvidia.com>
2017-07-31 01:35:34 +0530

    Output records in CSV format

--- entity_resolution.py
+++ entity_resolution.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from fuzzywuzzy import fuzz
-from pyspark.sql.types import *
+from pyspark.sql.types import IntegerType
 from pyspark.sql.functions import udf
 
 spark = SparkSession.builder.appName("EntityResolution").getOrCreate()
@@ -19,12 +19,31 @@ def fuzzy_ratio(col1, col2):
 fuzz_udf = udf(fuzzy_ratio,IntegerType())
 
 cond = [dblp_df.title == scholar_df.title, dblp_df.year == scholar_df.year]
-joined_fuzzy_df = dblp_df.join(scholar_df, cond).withColumn('fuzzy_ratio',fuzz_udf(dblp_df['authors'],
-                                                                                   scholar_df['authors']))
+joined_fuzzy_df = dblp_df.join(scholar_df, cond).withColumn("fuzzy_ratio",fuzz_udf(dblp_df["authors"],
+                                                                                   scholar_df["authors"]))
 
 results = joined_fuzzy_df.filter(joined_fuzzy_df.fuzzy_ratio > 80).collect()
 
-results.show(5,truncate=True)
+#results.show(5,truncate=True)
+
+'''
++--------------------+--------------------+--------------------+-------------+----+------+------------+--------------------+--------------------+--------------------+----+------+-----------+
+|              idDBLP|               title|             authors|        venue|year|Row_ID|   idScholar|               title|             authors|               venue|year|ROW_ID|fuzzy_ratio|
++--------------------+--------------------+--------------------+-------------+----+------+------------+--------------------+--------------------+--------------------+----+------+-----------+
+'''
+# Read Row objects and write to CSV in desired format
+with open("DBLP_Scholar_perfectMapping_rsomvanshi.csv", "w", encoding="utf-8") as f_out:
+    header = "idDBLP,idScholar,DBLP_Match,Scholar_Match,Match_ID"
+    f_out.write(header + "\n")
+    for result in results:
+        f_out.write(result[0] + "," + \
+                    result[6] + "," + \
+                    result[5] + "," + \
+                    # DBLP_Match : Row_ID
+                    result[11] + "," + \
+                    # Scholar_Match : ROW_ID
+                    result[5] +"_" + str(result[11]))
+        f_out.write("\n")
 
 # Stop the session
 spark.stop()
